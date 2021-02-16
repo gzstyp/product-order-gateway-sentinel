b282891ffb84029133a1df9ca7a2db6160f6f509
gzstyp <444141300@qq.com>
2021-02-16 21:33:35 +0800

    这个是不被其它服务调用

--- order-service/src/main/java/com/example/controller/OrderController.java
+++ order-service/src/main/java/com/example/controller/OrderController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @RestController
 @RequestMapping("/order")
@@ -20,5 +23,28 @@ public class OrderController{
     @GetMapping("/{id}")
     public Order selectProductById(@PathVariable("id") final Integer id){
         return orderService.selectOrderById(id);
+    }
+
+    //这个是不被其它服务调用 http://127.0.0.1:9090/order/index 或 http://127.0.0.1:9001/order/index
+    @GetMapping("/index")
+    public void index(final HttpServletResponse response){
+        final String json = "{\"code\":200,\"router\":\"order-service\",\"msg\":\"操作成功\"}";
+        responseJson(json,response);
+    }
+
+    public void responseJson(final String json,final HttpServletResponse response){
+        response.setContentType("text/html;charset=utf-8");
+        response.setHeader("Cache-Control","no-cache");
+        PrintWriter writer = null;
+        try {
+            writer = response.getWriter();
+            writer.write(json);
+            writer.flush();
+        }catch (final IOException ignored){}finally{
+            if(writer != null){
+                writer.close();
+                writer = null;
+            }
+        }
     }
 }
